04f12124a278e802911e2e84d04f69ba060d4e5d
Prachi Dhende <prachidhende09@gmail.com>
2026-02-11 17:31:11 +0530

    Add files via upload

---
+++ RevArray.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = 5;
+
+    int start = 0;
+    int end = n - 1;
+    int temp;
+
+    while (start < end) 
+    {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+
+    cout << "Reversed Array: ";
+    for (int i = 0; i < n; i++) 
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
